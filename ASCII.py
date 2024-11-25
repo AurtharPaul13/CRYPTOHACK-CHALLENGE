29d34170c90a44c0bb7179893cfb6d36fbe3dde3
B00177248 <b00177248@mytudublin.ie>
2024-11-25 15:55:01 +0000

    .

---
+++ ASCII.py
@@ -0,0 +1,10 @@
+ascii_int = [199, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
+
+#print("Here is your flag:")
+for char in ascii_int:
+  print(chr(char), end="")
+
+
+
+
+Solution - crypto{ASCII_pr1nt4bl3}
