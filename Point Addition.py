04f0c5da1fe17074f850394813e0dd17908c66a3
B00177248 <b00177248@mytudublin.ie>
2024-11-30 17:38:56 +0000

    Point Addition.py

---
+++ Point Addition.py
@@ -0,0 +1,19 @@
+from Crypto.Util.number import inverse
+
+def sum(P, Q, E):
+    O = (0, 0)
+    if P == O: return Q
+    elif Q == O: return P
+    else:
+        x1, y1 = P
+        x2, y2 = Q
+        if x1 == x2 and y1 == -y2: return O
+        Ea, Ep = E['a'], E['p']
+        if P != Q:
+            k = ((y2 - y1) * inverse(x2 - x1, Ep)) % Ep
+        else:
+            k = ((3*x1**2 + Ea) * inverse(2 * y1, Ep)) % Ep
+
+        x3 = (k**2 - x1 - x2) % Ep
+        y3 = (k*(x1 - x3) - y1) % Ep
+        return x3, y3
