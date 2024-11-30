ff3e1a0fba8ea46d85396a9ff26a4f55d08cc455
B00177248 <b00177248@mytudublin.ie>
2024-11-30 17:30:41 +0000

    Signing Server.py

---
+++ Signing Server.py
@@ -0,0 +1,49 @@
+from Crypto.Util.number import bytes_to_long, long_to_bytes
+from utils import listener
+
+
+
+class Challenge():
+    def __init__(self):
+        self.before_input = "Welcome to my signing server. You can get_pubkey, get_secret, or sign.\n"
+
+    def challenge(self, your_input):
+        if not 'option' in your_input:
+            return {"error": "You must send an option to this server"}
+
+        elif your_input['option'] == 'get_pubkey':
+            return {"N": hex(N), "e": hex(E) }
+
+        elif your_input['option'] == 'get_secret':
+            secret = bytes_to_long(SECRET_MESSAGE)
+            return {"secret": hex(pow(secret, E, N)) }
+
+        elif your_input['option'] == 'sign':
+            msg = int(your_input['msg'], 16)
+            return {"signature": hex(pow(msg, D, N)) }
+
+        else:
+            return {"error": "Invalid option"}
+
+      from pwn import *
+from json import *
+
+def send(hsh):
+    return r.sendline(dumps(hsh))
+r = remote('socket.cryptohack.org', 13374)
+r.recv()
+
+option =  {
+    'option': 'get_secret'
+}
+send(option)
+msg = loads(r.recv())["secret"]
+
+option =  {
+    'option': 'sign',
+    'msg': msg
+}
+send(option)
+get = loads(r.recv())
+message = bytes.fromhex(get["signature"][2:])
+print(message.decode())
