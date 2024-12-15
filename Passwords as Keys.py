7178a4dff8e3c815e1e600df5270e4b583f41f2d
B00177248 <b00177248@mytudublin.ie>
2024-12-15 04:45:42 +0000

    Update Passwords as Keys.py

--- Passwords as Keys.py
+++ Passwords as Keys.py
@@ -1,60 +1,26 @@
+from requests import get
+from json import loads
+from hashlib import md5
 from Crypto.Cipher import AES
-import hashlib
-import random
 
-with open("/usr/share/dict/words") as f:
-    words = [w.strip() for w in f.readlines()]
-keyword = random.choice(words)
-
-KEY = hashlib.md5(keyword.encode()).digest()
-FLAG = ?
-
-@chal.route('/passwords_as_keys/decrypt/<ciphertext>/<password_hash>/')
-def decrypt(ciphertext, password_hash):
-    ciphertext = bytes.fromhex(ciphertext)
-    key = bytes.fromhex(password_hash)
-
-    cipher = AES.new(key, AES.MODE_ECB)
-    try:
-        decrypted = cipher.decrypt(ciphertext)
-    except ValueError as e:
-        return {"error": str(e)}
-
-    return {"plaintext": decrypted.hex()}
-
-@chal.route('/passwords_as_keys/encrypt_flag/')
 def encrypt_flag():
-    cipher = AES.new(KEY, AES.MODE_ECB)
-    encrypted = cipher.encrypt(FLAG.encode())
+    url = 'https://aes.cryptohack.org/passwords_as_keys/encrypt_flag/'
+    r = get(url)
+    ct = (loads(r.text))['ciphertext']
+    return bytes.fromhex(ct)
 
-    return {"ciphertext": encrypted.hex()}
+gist = 'https://gist.githubusercontent.com/wchargin/8927565/raw/d9783627c731268fb2935a731a618aa8e95cf465/words'
+keys = get(gist).text.split("\n")
+ct = encrypt_flag()
 
-from Crypto.Cipher import AES
-import hashlib
-import random
-
-def decrypt(ciphertext, password_hash):
-    ciphertext = bytes.fromhex(ciphertext)
-    key = password_hash
-
+for key in keys:
+    key = md5(key.encode()).digest()
     cipher = AES.new(key, AES.MODE_ECB)
-    try:
-        decrypted = cipher.decrypt(ciphertext)
-    except ValueError as e:
-        return {"error": str(e)}
+    flag = cipher.decrypt(ct)
+    if(b'crypto' in flag):
+        print(flag.decode())
+        break
 
-    return decrypted
 
-with open("words.txt") as f:
-    words = [w.strip() for w in f.readlines()]
-keyword = random.choice(words)
 
-KEY = hashlib.md5(keyword.encode()).digest()
-
-l = len(words)
-
-for i in range(l):
-    KEY = hashlib.md5(words[i].encode()).digest()
-    decrypted = decrypt("c92b7734070205bdf6c0087a751466ec13ae15e6f1bcdd3f3a535ec0f4bbae66", KEY)
-    if b'crypto' in decrypted:
-        print(decrypted)
+The direction of the post is brute-force, the key is bluebell after running the loop.
