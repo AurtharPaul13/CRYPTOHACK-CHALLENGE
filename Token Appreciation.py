a531d10448fcca8366dd9f761491a7e2d53a04a9
B00177248 <b00177248@mytudublin.ie>
2024-12-15 08:19:11 +0000

    Update Token Appreciation.py

--- Token Appreciation.py
+++ Token Appreciation.py
@@ -4,3 +4,12 @@ jwt_encoded = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmbGFnIjoiY3J5cHRve2p3dF9j
 
 
 jwt.decode(jwt_encoded, options={"verify_signature": False})
+
+
+
+
+JWTs are broken to three parts separated by . the first part is the header, the second is the payload, and the last one is the signature.
+
+Both the header and payload can easily be decrypted to see its plaintext value. The signature, on the other hand, is encrypted with a 256-bit secret key.
+
+You can use online tools like this one in order to decode JWTs but since the challenge recommended that we use PyJWT in order to prepare us for future challenges, we’re going to use that.
