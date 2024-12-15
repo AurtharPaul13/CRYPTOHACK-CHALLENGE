dd1cb383da4172ecd07f0302fefe8483312aa2fc
B00177248 <b00177248@mytudublin.ie>
2024-12-15 04:54:47 +0000

    Update Bean Counter.py

--- Bean Counter.py
+++ Bean Counter.py
@@ -1,46 +1,3 @@
-from Crypto.Cipher import AES
-
-
-KEY = ?
-
-
-class StepUpCounter(object):
-    def __init__(self, step_up=False):
-        self.value = os.urandom(16).hex()
-        self.step = 1
-        self.stup = step_up
-
-    def increment(self):
-        if self.stup:
-            self.newIV = hex(int(self.value, 16) + self.step)
-        else:
-            self.newIV = hex(int(self.value, 16) - self.stup)
-        self.value = self.newIV[2:len(self.newIV)]
-        return bytes.fromhex(self.value.zfill(32))
-
-    def __repr__(self):
-        self.increment()
-        return self.value
-
-
-
-@chal.route('/bean_counter/encrypt/')
-def encrypt():
-    cipher = AES.new(KEY, AES.MODE_ECB)
-    ctr = StepUpCounter()
-
-    out = []
-    with open("challenge_files/bean_flag.png", 'rb') as f:
-        block = f.read(16)
-        while block:
-            keystream = cipher.encrypt(ctr.increment())
-            xored = [a^b for a, b in zip(block, keystream)]
-            out.append(bytes(xored).hex())
-            block = f.read(16)
-
-    return {"encrypted": ''.join(out)}
-
-
 from requests import get
 from PIL import Image
 from json import loads
@@ -62,3 +19,8 @@ png_flag = xor(keystream, ct)
 image = open('flag.png', 'wb').write(png_flag)
 flag = Image.open('flag.png')
 flag.show()
+
+
+First, we need to analyze the source code of the article, you can see. step_up=False(=0) Leads to self.newIV = hex(int(self.value, 16) Or or iv does not change in the coding process, causing keystream become a 16 bytes series repeated several times and then xor with clear version.
+We can find these 16 bytes. First we have keysream(16) ^ flag(??) = ct(??) should keystream(16) ^ flag(16) = ct(16)
+We are interested in the first 16 bytes of the flag because the first 16 bytes of the PNG file is always b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR'. I know the first 16 bytes. ct, then we found keystream. 
