337c4e48a7d46bf25503f2b91a9a6c07a138b5d0
B00177248 <b00177248@mytudublin.ie>
2024-12-15 05:31:18 +0000

    Update ASCII.py

--- ASCII.py
+++ ASCII.py
@@ -8,3 +8,12 @@ for char in ascii_int:
 
 
 Solution - crypto{ASCII_pr1nt4bl3}
+
+
+
+ASCII is an encoding scheme wherein each character has a designated integer value. This is especially useful for translating strings to other formats like hexadecimal, binary, or bytes.
+
+You can solve this using online tools such as CyberChef, However, in order to hone our programming skills, which we would definitely need later on, we’re going to solve this using python
+The code above uses the chr() function of Python. This converts the integer to its character value based on the ASCII matrix.
+
+So basically, 199 = c, 114 = r, 121 = y, and so on. Important to remember that every character has its own unique integer value so lower and uppercase letters have different values
