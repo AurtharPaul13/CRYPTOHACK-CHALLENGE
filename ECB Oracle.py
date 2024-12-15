a148710f2290f0bb5da02dd40125c420a18857f9
B00177248 <b00177248@mytudublin.ie>
2024-12-15 04:48:15 +0000

    Update ECB Oracle.py

--- ECB Oracle.py
+++ ECB Oracle.py
@@ -24,3 +24,11 @@ for i in range(8, 26):
             print(FLAG)
             break 
 assert len(FLAG) == LEN
+
+
+we need to understand the padding mechanism of the code above. The code will automatically buffer the bytes so that the block length is a multiple of 16, here we can determine the length of the flag.
+    Running a simple code as above, we see with value i = 6 then len(get) = 32 , i = 7 then len(get) = 48. That's why, len(FLAG) Satisfiction 7 + len(FLAG) = 32 should len(FLAG) = 25. Fortunately, the mode was chosen as the ECB, so the job is much simpler. Besides, we can use it. len(FLAG) to assert later
+
+    
+
+
