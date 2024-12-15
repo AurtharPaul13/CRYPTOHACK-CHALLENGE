2fd769eb6dc1b673308f44c91cc604b9e8f8cbf9
B00177248 <b00177248@mytudublin.ie>
2024-12-15 08:22:42 +0000

    Update No Way Jose.py

--- No Way Jose.py
+++ No Way Jose.py
@@ -45,3 +45,14 @@ def authorise(token):
 def create_session(username):
     encoded = jwt.encode({'username': username, 'admin': False}, SECRET_KEY, algorithm='HS256')
     return {"session": encoded}
+
+
+As mentioned on the first challenge, Token Appreciation, the first part of JWT is the JOSE header.
+
+This is where you indicate that this is a JWT and what algorithm was used for the encryption. According to the challenge, this is a major flaw since a server have to process this untrusted payload before verifying the JWT as a whole, enabling us to essentially bypass authorization by manipulating the contents of this header.
+
+The No Way Jose challenge will present you with a code. You can play with it using different usernames and see if you can be verified as an admin.
+
+First, generate a JWT using the admin username. If we decode the generated JWT, it will do the encode and decode ad header and Payload.The nice thing about jwt.io is that we can edit the contents of the payload. So, change the admin value to true
+
+
