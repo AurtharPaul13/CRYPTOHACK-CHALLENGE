cf7e113df8d5cc449c06940cba5abeb0b94716f0
B00177248 <b00177248@mytudublin.ie>
2024-12-15 05:35:48 +0000

    Update You_either_know__XOR_you_dont.py

--- You_either_know__XOR_you_dont.py
+++ You_either_know__XOR_you_dont.py
@@ -12,3 +12,15 @@ complete_key = (partial_key * (len(message)//len(partial_key)+1))[:len(message)]
 flag = xor(message, complete_key)
 
 print(flag.decode())
+
+
+
+Let’s consider the given value 0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104 as the “message” and crypto{ as the flag. We’re looking for the secret key so the partial formula should look like this
+
+message ^ secret key = “crypto{“
+
+And in order to get to get the secret key, we should do this
+
+message ^ “crypto{“ = partial secret key
+
+As I said, XOR working by individual value comparison. Since we only have the first seven characters of the flag (“crypto{“), we have to XOR that to the first 7 values of the message in order to get the first 7 character of the secret key. We will do that by using the associative property of XOR
