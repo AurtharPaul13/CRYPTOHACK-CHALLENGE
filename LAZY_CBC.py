57f93388256a8cea928da183ef3edfdb011f42ec
B00177248 <b00177248@mytudublin.ie>
2024-12-15 04:51:08 +0000

    Update LAZY_CBC.py

--- LAZY_CBC.py
+++ LAZY_CBC.py
@@ -1,70 +1,36 @@
-from Crypto.Cipher import AES
-
-KEY = ?
-FLAG = ?
-
-@chal.route('/lazy_cbc/encrypt/<plaintext>/')
-def encrypt(plaintext):
-    plaintext = bytes.fromhex(plaintext)
-    if len(plaintext) % 16 != 0:
-        return {"error": "Data length must be multiple of 16"}
-
-    cipher = AES.new(KEY, AES.MODE_CBC, KEY)
-    encrypted = cipher.encrypt(plaintext)
-
-    return {"ciphertext": encrypted.hex()}
-
-@chal.route('/lazy_cbc/get_flag/<key>/')
-def get_flag(key):
-    key = bytes.fromhex(key)
-
-    if key == KEY:
-        return {"plaintext": FLAG.encode().hex()}
-    else:
-        return {"error": "invalid key"}
-
-@chal.route('/lazy_cbc/receive/<ciphertext>/')
-def receive(ciphertext):
-    ciphertext = bytes.fromhex(ciphertext)
-    if len(ciphertext) % 16 != 0:
-        return {"error": "Data length must be multiple of 16"}
-
-    cipher = AES.new(KEY, AES.MODE_CBC, KEY)
-    decrypted = cipher.decrypt(ciphertext)
-
-    try:
-        decrypted.decode() # ensure plaintext is valid ascii
-    except UnicodeDecodeError:
-        return {"error": "Invalid plaintext: " + decrypted.hex()}
-
-    return {"success": "Your message has been received"}
-
+from pwn import *
 import requests
-from Crypto.Util.Padding import pad, unpad
-from Crypto.Util.number import long_to_bytes, bytes_to_long
+from json import loads
+
+def encrypt(pt):
+    pt = bytes.hex(pt)
+    url = f"http://aes.cryptohack.org/lazy_cbc/encrypt/{pt}/"
+    r = requests.get(url)
+    enc = (loads(r.text))['ciphertext']
+    return enc
+
+def receive(ct):
+    url = f"http://aes.cryptohack.org/lazy_cbc/receive/{ct}/"
+    r = requests.get(url)
+    pt = loads(r.text)['error'].split(": ")[1]
+    return pt
 
 def get_flag(key):
-    url = "http://aes.cryptohack.org/lazy_cbc/get_flag/"
-    url += key.hex()
-    url += "/"
+    key = bytes.hex(key)
+    url = f"http://aes.cryptohack.org/lazy_cbc/get_flag/{key}/"
     r = requests.get(url)
-    js = r.json()
-    return bytes.fromhex(js["plaintext"])
+    flag = (loads(r.text))['plaintext']
+    return flag
 
-def response(ciphertext):
-    url = "http://aes.cryptohack.org/lazy_cbc/receive/"
-    url += ciphertext.hex()
-    url += "/"
-    r = requests.get(url)
-    js = r.json()
-    return bytes.fromhex(js["error"][len("Invalid plaintext: "):])
+pt1 = b'\x00'*16
+pt2 = b'\x00'*32
+ct1, ct2 = encrypt(pt1), encrypt(pt2)
+ct2 = ct2[len(ct1):]
+temp = receive(ct2)
+key = xor(bytes.fromhex(temp), bytes.fromhex(ct1))
+flag = get_flag(key)
+print(bytes.fromhex(flag).decode())
 
-def xor(a, b):
-    return long_to_bytes(bytes_to_long(a) ^ bytes_to_long(b))
 
-ciphertext = b"\x00" * 32
-
-CD = response(ciphertext)
-C = CD[:16]
-D = CD[16:]
-print(get_flag(xor(C, D)))
+we need to know the coding diagram of the CBC mode.Our formula overview will be: C = E(key, xor(P, key)). So, if you choose P = b'\x00'*16, we can shorten the city C = E(key, key).
+we need to adjust a little bit. Part ct2 Really is ct2[len(ct1):], part pt2 It's really pt2[len(pt1):]based on the coded diagram above. Here, we have transformations.Get the value of temp, I calculate key = xor(temp, ct1) And submit on the web to get the flag easily
