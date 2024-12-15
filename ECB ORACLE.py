df8b876b1994f7d4f5ed3768eb1f52ea6d3eed07
B00177248 <b00177248@mytudublin.ie>
2024-12-15 08:16:44 +0000

    Update ECB ORACLE.py

--- ECB ORACLE.py
+++ ECB ORACLE.py
@@ -30,3 +30,16 @@ for i in range(7, 26):
             flag += j.to_bytes(1, byteorder="big")
             print(flag)
             break
+
+
+The input will be plus the flag at the back to the out put is the encoding of (input + flag).
+
+The ECB will encode each block of 16 bytes.
+
+We’ll start with a block with 31 bytes ending with “crypte{”
+
+⇒ b’xxxxxxxcrypto{’ + 1 bytes
+
+So we’ll brute force to find out the last bytes by comparing the encrypted version after transmitt. ‘…xxxxxxxcrypto{’ + 1bytes into the encoding board
+
+After finding 1 character, we will use the same way to find the next character until the complete flag is obtained.
