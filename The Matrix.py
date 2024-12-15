5089c70d56acedf046af530c8f4aba4dfa1d3de1
B00177248 <b00177248@mytudublin.ie>
2024-12-15 10:26:36 +0000

    Update The Matrix.py

--- The Matrix.py
+++ The Matrix.py
@@ -34,3 +34,21 @@ mat = generate_mat()
 
 ciphertext = mat^E
 save_matrix(ciphertext, 'flag.enc')
+
+
+
+    I will first try to analyze the coding algorithm of the article. For
+
+P = 2
+N = 50
+E = 31337
+
+with N is the size of the matrix, in other words the matrix of the card is 50x50. The matrix will be placed in school. Fp with P = 2, almost identical to the results of XOR:
+
+Trong F2 thì:
+1 + 1 = 0
+1 + 0 = 1
+0 + 0 = 0
+0 + 1 = 1
+
+    The encryption process is as follows. The first flag will be converted into a binary matrix mat. If the length of flag Not larger than the 50x50. mat will be cushioned to add the number 0, 1 random at the end. Then with a mess code. mat become the matrix of its own transposition.
