b1698f5db71c3243cbbe1da302ab332ab08655e8
B00177248 <b00177248@mytudublin.ie>
2024-12-15 08:25:35 +0000

    Update Diffie-Hellman Starter 2.py

--- Diffie-Hellman Starter 2.py
+++ Diffie-Hellman Starter 2.py
@@ -9,3 +9,9 @@ for g in range(p):
     if check(g, p):
         print(g)
         break
+
+
+Speak long lines of writing as above, but actually the element of birth. g of FpSatisfied with the following properties:
+g^1 != g^2 != g^3 != ... != g^(p-1) (mod p)
+My main goal is to avoid the birth of a cyclic subgroup
+
