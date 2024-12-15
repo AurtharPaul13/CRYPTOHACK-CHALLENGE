700c2d4101288de0636c42f3945e59f6e2d005a9
B00177248 <b00177248@mytudublin.ie>
2024-12-15 05:17:05 +0000

    Update Everything is Big.py

--- Everything is Big.py
+++ Everything is Big.py
@@ -1,29 +1,3 @@
-from Crypto.Util.number import getPrime, bytes_to_long
-
-FLAG = b"crypto{?????????????????????????}"
-
-m = bytes_to_long(FLAG)
-
-def get_huge_RSA():
-    p = getPrime(1024)
-    q = getPrime(1024)
-    N = p*q
-    phi = (p-1)*(q-1)
-    while True:
-        d = getPrime(256)
-        e = pow(d,-1,phi)
-        if e.bit_length() == N.bit_length():
-            break
-    return N,e
-
-
-N, e = get_huge_RSA()
-c = pow(m, e, N)
-
-print(f'N = {hex(N)}')
-print(f'e = {hex(e)}')
-print(f'c = {hex(c)}')
-
 from gmpy2 import iroot
 from Crypto.Util.number import long_to_bytes
 from fractions import Fraction
@@ -82,3 +56,7 @@ p, q, d = crack(e, N)
 
 pt = long_to_bytes(pow(c, d, N))
 print(pt)
+
+
+With two numbers N, e Terrorism like that, we can guess the number. d This will be relatively small, this is the condition to use Weiner’s attack
+
